ea82cedad2450e73ed38a70c4c393ccbfe5e812c
Can Zhang <raitorm@gmail.com>
2016-05-10 21:29:55 -0400

    Fix lint problems

--- kudos/backend.py
+++ kudos/backend.py
@@ -1,14 +1,15 @@
 class DuplicateEmailError(Exception):
     pass
 
+
 class UserBackend(object):
-    
+
     def __init__(self):
         self._user_list = []
 
     def create_user(self, email):
-        
+
         if email in self._user_list:
             raise DuplicateEmailError
-            
+
         self._user_list.append(email)
