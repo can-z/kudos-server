1e3e386e4215760aba35233677a3ff6e37499681
Can Zhang <raitorm@gmail.com>
2016-08-29 21:41:16 -0400

    Add integration test example

---
+++ tests/integration/test_example.py
@@ -0,0 +1,11 @@
+import unittest
+import kudos.views as kudos
+
+
+class IntegrationTestCase(unittest.TestCase):
+    def setUp(self):
+        self.app = kudos.app.test_client()
+
+    def test_example_end_point(self):
+        result = self.app.get("/")
+        self.assertEqual(result.data, "Hello world!")
