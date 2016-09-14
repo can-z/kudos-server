5163ee18a966f9d7c8bf5f5dbfcf903e41131df2
Can Zhang <raitorm@gmail.com>
2016-09-14 19:22:02 -0400

    Fix styles

--- tests/integration/test_example.py
+++ tests/integration/test_example.py
@@ -2,9 +2,11 @@ import pytest
 import six
 import kudos.views as kudos
 
+
 @pytest.fixture
 def create_client():
     return kudos.app.test_client()
+
 
 def test_example_end_point(create_client):
     app = create_client
--- tests/test_backend.py
+++ tests/test_backend.py
@@ -13,6 +13,7 @@ def test_create_user_adds_multiple_elements():
     sut.create_user("andrew@gmail.com")
     assert len(sut._user_list) == 2
 
+
 def test_create_different_user():
     sut = UserBackend()
     email = "john@gmail.com"
@@ -24,12 +25,14 @@ def test_create_different_user():
 
     assert sut._user_list[0] != sut._user_list[1]
 
+
 def test_it_does_not_allow_duplicate_emails():
     sut = UserBackend()
     email = "john@gmail.com"
     sut.create_user(email)
     with pytest.raises(DuplicateEmailError):
         sut.create_user(email)
+
 
 def test_invalid_email_format():
     sut = UserBackend()
