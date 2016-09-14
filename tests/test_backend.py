7dc2b21046728d8d1c095cfbb8a21230d9bb1cd5
Can Zhang <raitorm@gmail.com>
2016-09-14 19:13:04 -0400

    Replace nosetests syntax with pytest

--- tests/test_backend.py
+++ tests/test_backend.py
@@ -1,4 +1,4 @@
-import unittest
+import pytest
 from kudos.backend import (
     DuplicateEmailError,
     InvalidEmailError,
@@ -6,34 +6,33 @@ from kudos.backend import (
 )
 
 
-class UserBackendTestCase(unittest.TestCase):
-    def test_create_user_adds_multiple_elements(self):
-        sut = UserBackend()
-        sut.create_user("john@gmail.com")
-        self.assertEqual(len(sut._user_list), 1)
-        sut.create_user("andrew@gmail.com")
-        self.assertEqual(len(sut._user_list), 2)
+def test_create_user_adds_multiple_elements():
+    sut = UserBackend()
+    sut.create_user("john@gmail.com")
+    assert len(sut._user_list) == 1
+    sut.create_user("andrew@gmail.com")
+    assert len(sut._user_list) == 2
 
-    def test_create_different_user(self):
-        sut = UserBackend()
-        email = "john@gmail.com"
+def test_create_different_user():
+    sut = UserBackend()
+    email = "john@gmail.com"
+    sut.create_user(email)
+    sut._user_list[0] == email
+
+    email_2 = "andrew@gmail.com"
+    sut.create_user(email_2)
+
+    assert sut._user_list[0] != sut._user_list[1]
+
+def test_it_does_not_allow_duplicate_emails():
+    sut = UserBackend()
+    email = "john@gmail.com"
+    sut.create_user(email)
+    with pytest.raises(DuplicateEmailError):
         sut.create_user(email)
-        self.assertEqual(sut._user_list[0], email)
 
-        email_2 = "andrew@gmail.com"
-        sut.create_user(email_2)
-
-        self.assertTrue(sut._user_list[0] != sut._user_list[1])
-
-    def test_it_does_not_allow_duplicate_emails(self):
-        sut = UserBackend()
-        email = "john@gmail.com"
+def test_invalid_email_format():
+    sut = UserBackend()
+    email = "invalidEmail@email_address"
+    with pytest.raises(InvalidEmailError):
         sut.create_user(email)
-        with self.assertRaises(DuplicateEmailError):
-            sut.create_user(email)
-
-    def test_invalid_email_format(self):
-        sut = UserBackend()
-        email = "invalidEmail@email_address"
-        with self.assertRaises(InvalidEmailError):
-            sut.create_user(email)
