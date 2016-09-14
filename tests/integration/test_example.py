8532c0aac2d4c7210fa4f9e9ccaa5de7ebbc0524
Can Zhang <raitorm@gmail.com>
2016-09-14 19:16:02 -0400

    Try pytest fixture syntax

--- tests/integration/test_example.py
+++ tests/integration/test_example.py
@@ -1,8 +1,12 @@
+import pytest
 import six
 import kudos.views as kudos
 
+@pytest.fixture
+def create_client():
+    return kudos.app.test_client()
 
-def test_example_end_point():
-    app = kudos.app.test_client()
+def test_example_end_point(create_client):
+    app = create_client
     result = app.get("/")
     assert result.data == six.b("Hello World!")
